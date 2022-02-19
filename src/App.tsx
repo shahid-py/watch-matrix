52bb9370014e5d3ecf80caea11b9ea641ec9145c
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-19 17:16:55 +0530

    chore: installation of react app with typescript template

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         hello matrix
+         
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
