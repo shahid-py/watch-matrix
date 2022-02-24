329283fdd2cae3c9bd97954ea6eac061ced619ae
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-24 11:39:29 +0530

    feat: implemented basic feature on frontend

---
+++ src/context/DataContext/DataContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useEffect, useReducer } from "react";
+import { DataContextTypes } from "./dataContext.types";
+import { reducerFunc, initialState } from "../../reducer/reducer";
+import axios from "axios";
+
+import { API_URL } from "../../utils/index";
+
+export type DataProviderProps = {
+    children: React.ReactNode;
+  };
+  
+  export const DataContext = createContext<DataContextTypes>({
+    state: initialState,
+    dispatch: () => null,
+  });
+  export const DataProvider = ({ children }: DataProviderProps) => {
+    const [state, dispatch] = useReducer(reducerFunc, initialState);
+    
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const {
+          data: { success, videos },
+        } = await axios.get(`${API_URL}/videos`);
+        console.log({ success });
+        if (success) {
+          dispatch({ type: "INITIALIZE_VIDEOS", payload: videos });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <DataContext.Provider value={{ state, dispatch }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+export const useData = () => useContext(DataContext);
