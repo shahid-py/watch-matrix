619ab6e58411e5199c627a4f380b563ad2a77f50
Mohd Shahid Khan <khan42290@gmail.com>
2022-02-24 11:42:07 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 
 
 import './App.css';
+import { Header } from "./components/Header/Header"
+import { Routes,Route } from "react-router-dom";
+import {
+  Home }
+  from "./pages/index"
+  import {ScrollToTop} from "./components/ScrollToTop"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         hello matrix
-         
-        
-      </header>
+    <div>
+     <ScrollToTop />
+      <Header />
+
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+      
     </div>
   );
 }
