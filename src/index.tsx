85b1a25345af230f2fd9283d9ebb6e3294e1a1e0
Mohd Shahid Khan <khan42290@gmail.com>
2022-03-03 16:46:45 +0530

    feat : integrated JWT authentication for backend operation

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,21 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import { DataProvider } from "./context/DataContext/DataContext";
+import { AuthProvider } from './context/AuthContext/AuthContext';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <AuthProvider>
     <DataProvider>
     <App />
     </DataProvider>
+    </AuthProvider>
     </Router>
     
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
