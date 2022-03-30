c09c54d42192a3f889f3aa64c34b91e5a974dfea
Mohd Shahid Khan <khan42290@gmail.com>
2022-03-30 14:45:27 +0530

    refactor

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,8 @@
 export { History } from "./History/History"
 export { Home } from "./Home/Home"
 export { Signup } from "./Signup/Signup"
+export { LikedVideos } from "./Liked/Liked"
 export { Login } from "./Login/Login"
+export { PlaylistDetails } from "./PlaylistDetails/PlaylistDetails";
+export { Playlists } from "./Playlists/Playlists"
+export { VideoDetails } from "./VideoDetails/VideoDetails"
